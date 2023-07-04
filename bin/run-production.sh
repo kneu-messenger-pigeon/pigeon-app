3ea2ca8002e1fbb17c6899623d411e7fee39121c
Anton Berezhnyi <aberezhniy@clips4sale.com>
2023-07-04 23:26:47 +0300

    Imporve deploy production

--- bin/run-production.sh
+++ bin/run-production.sh
@@ -4,10 +4,10 @@ set -e
 stat docker-compose.prod.yml > /dev/null
 [ -f  docker-compose.yml ] || ln -s docker-compose.prod.yml docker-compose.yml
 
-docker compose pull
-docker compose build
+docker compose pull --quiet
+docker compose build --quiet
 docker compose down
 docker compose up -d
 
 sleep 5
-curl --fail  http://$(docker compose port authorizer 80)/healthcheck
+curl --fail  http://"$(docker compose port authorizer 80)"/healthcheck
