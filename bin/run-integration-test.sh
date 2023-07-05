3197a083a4d438969919603dfd83926ad6932b2e
Anton Berezhnyi <aberezhniy@clips4sale.com>
2023-07-05 13:54:59 +0300

    Detect workflow calling

--- bin/run-integration-test.sh
+++ bin/run-integration-test.sh
@@ -1,11 +1,10 @@
 #!/usr/bin/env sh
-set -e
 DOCKER_COMPOSE="docker compose -f docker-compose.prod.yml -f docker-compose.integration-test.yml -p pigeon-app-integration-test "
 
-$DOCKER_COMPOSE stop > /dev/null || true
+$DOCKER_COMPOSE down --volumes --timeout 30 >/dev/null 2>&1
 $DOCKER_COMPOSE up --renew-anon-volumes --pull --build --force-recreate --timeout 1200 --attach init-kafka --exit-code-from init-kafka
 EXIT_CODE=$?
 echo $EXIT_CODE
-$DOCKER_COMPOSE down --volumes --timeout 30 2> /dev/null || true
+$DOCKER_COMPOSE down --volumes --timeout 60 >/dev/null 2>&1
 
 exit $EXIT_CODE
