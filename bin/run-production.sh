1ab371236e84e573a8f60d6b45ebe16607977a47
Anton Berezhnyi <aberezhniy@clips4sale.com>
2023-07-17 12:26:14 +0300

    Modify prod

--- bin/run-production.sh
+++ bin/run-production.sh
@@ -1,6 +1,17 @@
 #!/usr/bin/env sh
 set -e
 
+if [ -z "$AUTHORIZER_PORT" ] && command -v yq >/dev/null 2>&1; then
+  if [ -f docker-compose.prod.yml ]; then
+    AUTHORIZER_PORT=$(yq -e '.services.authorizer.ports[0]' docker-compose.prod.yml )
+  else
+    AUTHORIZER_PORT=$(yq -e '.services.authorizer.ports[0]' docker-compose.yml )
+  fi
+
+  AUTHORIZER_PORT=$(echo "$AUTHORIZER_PORT" | cut -d':' -f2)
+  echo "Authorizer port: $AUTHORIZER_PORT"
+fi
+
 docker compose pull --quiet
 docker compose build --quiet
 docker compose down --remove-orphans
