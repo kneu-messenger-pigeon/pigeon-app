cd6cbcc473988402443c1a9d281683d950031eac
Anton Berezhnyi <aberezhniy@clips4sale.com>
2023-07-17 11:34:39 +0300

    Use infisical outside docker

--- bin/run-integration-test.sh
+++ bin/run-integration-test.sh
@@ -4,7 +4,7 @@ which infisical || exit 20
 
 export INFISICAL_DISABLE_UPDATE_CHECK=true
 VARS=$(infisical  export --env=integration-testing) || exit 25
-export "$( xargs <<< "$VARS" )"
+export "$( echo "$VARS" | xargs  )"
 
 $DOCKER_COMPOSE down --volumes --timeout 30 >/dev/null 2>&1
 $DOCKER_COMPOSE up --renew-anon-volumes --pull --build --force-recreate --timeout 1200 --attach init-kafka --exit-code-from init-kafka
