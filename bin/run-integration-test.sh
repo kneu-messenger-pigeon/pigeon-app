a12d611c07f6f1ad4f945feffa07ff059dbca944
Anton Berezhnyi <aberezhniy@clips4sale.com>
2023-07-17 11:34:39 +0300

    Use infisical outside docker

--- bin/run-integration-test.sh
+++ bin/run-integration-test.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env sh
 DOCKER_COMPOSE="docker compose -f docker-compose.prod.yml -f docker-compose.integration-test.yml -p pigeon-app-integration-test "
 which infisical || exit 20
-$DOCKER_COMPOSE down --volumes --timeout 30 >/dev/null 2>&1
 
-infisical run --env=integration-testing -- \
+export INFISICAL_DISABLE_UPDATE_CHECK=true
+VARS=$(infisical  export --env=integration-testing) || exit 25
+export "$( xargs <<< "$VARS" )"
+
+$DOCKER_COMPOSE down --volumes --timeout 30 >/dev/null 2>&1
 $DOCKER_COMPOSE up --renew-anon-volumes --pull --build --force-recreate --timeout 1200 --attach init-kafka --exit-code-from init-kafka
 
 EXIT_CODE=$?
