52c63457f4cda902d2ed7e196796fd8d939981bf
Anton Berezhnyi <aberezhniy@clips4sale.com>
2023-07-22 18:41:49 +0300

    Fix integration test

--- bin/run-integration-test.sh
+++ bin/run-integration-test.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 DOCKER_COMPOSE="docker compose -f docker-compose.prod.yml -f docker-compose.integration-test.yml -p pigeon-app-integration-test "
 
-$DOCKER_COMPOSE down --remove-orphans --volumes --timeout 30 >/dev/null 2>&1
+$DOCKER_COMPOSE rm  --stop --force --volumes >/dev/null 2>&1
 $DOCKER_COMPOSE up --renew-anon-volumes --pull --build --force-recreate --timeout 1200 --attach init-kafka --exit-code-from init-kafka
 
 EXIT_CODE=$?
