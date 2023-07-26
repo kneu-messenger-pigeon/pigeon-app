e42ddd99b7c22c68d84a0c33a6558c61f730e06e
Anton Berezhnyi <aberezhniy@clips4sale.com>
2023-07-26 12:51:36 +0300

    Add timeouts

--- bin/run-production.sh
+++ bin/run-production.sh
@@ -3,7 +3,7 @@ set -e
 
 docker compose pull --quiet
 docker compose build --quiet
-docker compose up -d --remove-orphans
+docker compose up -d --remove-orphans --timeout 180
 
 echo ""
 sleep 4
@@ -12,4 +12,5 @@ docker compose ps
 
 PORT=$(docker compose port gateway 443)
 echo "Healthcheck authorizer via gateway: $PORT"
-curl --fail -I https://"${PORT:-unknown}"/authorizer/healthcheck && echo "Authorizer is up" || exit 1
+curl  --url https://"${PORT:-unknown}"/authorizer/healthcheck --fail -I --connection-timeout 3 --max-time 5 && \
+ echo "Authorizer is up" || exit 1
