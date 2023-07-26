bf16b7b80c03fcc303f515a6248e62df1fdd93e7
Anton Berezhnyi <aberezhniy@clips4sale.com>
2023-07-26 17:24:07 +0300

    Imrpove echo

--- bin/run-production.sh
+++ bin/run-production.sh
@@ -16,13 +16,13 @@ export HEALTHCHECK_URI="/authorizer/healthcheck"
 HTTP_PORT=$(docker compose port gateway 80)
 echo "Healthcheck authorizer via gateway with HTTP: $HTTP_PORT"
 curl  --url "http://${HTTP_PORT:-unknown}${HEALTHCHECK_URI}" --fail -i --max-time 5 && \
- echo "Authorizer and gateway is up (http: ok)" || exit 1
+  echo "" && echo "Authorizer and gateway is up (http: ok)" || exit 1
 
 HTTPS_PORT=$(docker compose port gateway 443)
 echo "Healthcheck authorizer via gateway with HTTPS: $HTTPS_PORT"
 curl  --url "https://${HTTPS_PORT:-unknown}${HEALTHCHECK_URI}" --fail -i --max-time 5 --insecure && \
- echo "Authorizer and gateway is up (https: ok)" || exit 1
+  echo "" && echo "Authorizer and gateway is up (https: ok)" || exit 1
 
 echo "Healthcheck authorizer via gateway with Public URL: $PUBLIC_URL"
 curl  --url "${PUBLIC_URL}${HEALTHCHECK_URI}" --fail -i --max-time 5 && \
- echo "Authorizer and gateway is up (public: ok)" || exit 1
+  echo "" && echo "Authorizer and gateway is up (public: ok)" || exit 1
