b855b76be7b9dda495cc41c37be233500ff59d3e
Anton Berezhnyi <aberezhniy@clips4sale.com>
2023-08-25 13:18:44 +0300

    Implement integration service container

--- bin/run-integration-test.sh
+++ bin/run-integration-test.sh
@@ -4,7 +4,7 @@
 DOCKER_COMPOSE="docker compose -f docker-compose.prod.yml -f docker-compose.integration-test.yml -p pigeon-app-integration-test "
 
 $DOCKER_COMPOSE rm  --stop --force --volumes >/dev/null 2>&1
-$DOCKER_COMPOSE up --renew-anon-volumes --pull --build --force-recreate --timeout 1200 --attach init-kafka --exit-code-from init-kafka
+$DOCKER_COMPOSE up --renew-anon-volumes --pull --build --force-recreate --timeout 1200 --attach integration-testing --exit-code-from integration-testing
 
 EXIT_CODE=$?
 echo $EXIT_CODE
