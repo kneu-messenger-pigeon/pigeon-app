5cb2580e25d6d3777517305c4fa0100adb3edf9c
Anton Berezhnyi <aberezhniy@clips4sale.com>
2023-09-09 18:15:44 +0300

    Improve integrion testing launch options

--- bin/dev-run-integration-test-at-host-machine.sh
+++ bin/dev-run-integration-test-at-host-machine.sh
@@ -4,8 +4,11 @@
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 cd $SCRIPT_DIR/..
 
-DOCKER_COMPOSE="docker compose -f docker-compose.prod.yml -f docker-compose.integration-test.yml \
--p pigeon-app-integration-test --env-file .env --env-file .env.integration-test-at-host"
+DOCKER_COMPOSE="docker compose -f docker-compose.base.yml -f docker-compose.integration-test.yml \
+-p pigeon-app-integration-test --profile kafka-ui-enabled \
+--env-file .env --env-file .env.integration-test-at-host"
 
-$DOCKER_COMPOSE rm  --stop --force --volumes
-$DOCKER_COMPOSE up --renew-anon-volumes --pull --build -d --scale integration-testing=0
+
+$DOCKER_COMPOSE down --volumes --remove-orphans --timeout 30
+$DOCKER_COMPOSE up --pull --build --force-recreate -d --scale integration-testing=0
+
